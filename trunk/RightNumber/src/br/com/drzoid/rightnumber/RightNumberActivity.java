c450a8c6444972bb836f79c64832091fa34de49c
rdamazio <rdamazio@58f3d768-a58c-e14e-36d0-260f8ed6ecdc>
2010-04-06 17:50:48 +0000

    Initial import

---
+++ trunk/RightNumber/src/br/com/drzoid/rightnumber/RightNumberActivity.java
@@ -0,0 +1,17 @@
+package br.com.drzoid.rightnumber;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * The main activity, which shows RightNumber preferences.
+ */
+public class RightNumberActivity extends Activity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // TODO: Show preferences
+    setContentView(R.layout.main);
+  }
+}
