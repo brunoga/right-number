b43aa339a9e8b11a10a0861b3928c592f2b23ca1
rdamazio <rdamazio@58f3d768-a58c-e14e-36d0-260f8ed6ecdc>
2010-04-07 10:11:04 +0000

    Preparing to support more countries which have carrier-based dialing

--- RightNumber/src/br/com/drzoid/rightnumber/PhoneNumberFormatter.java
+++ RightNumber/src/br/com/drzoid/rightnumber/PhoneNumberFormatter.java
@@ -89,4 +89,80 @@ public class PhoneNumberFormatter {
     }
     return newNumber;
   }
+
+  // Info from http://www.exportbureau.com/telephone_codes/international_dialcode.html
+  // Common patterns: 0(national)/00(international)+carrier, 0+carrier (intl), 00+carrier (intl), carrier+00 (intl), 
+  // TODO: Implement these as well
+  // TODO: Check that for all other countries which only take 0/00/011 but no carrier it already gives the right number
+  // TODO: Check which countris support dialing with + instead of the IDD
+  // Other relevant resources:
+  // http://countrycode.org/
+  // http://www.timedial.net/world-time-zone-dialing-codes-from-VE.aspx
+  // http://en.wikipedia.org/wiki/List_of_international_call_prefixes
+  // http://en.wikipedia.org/wiki/List_of_country_calling_codes
+  // http://www.howtocallabroad.com/
+
+  private String reformatForBolivia(PhoneNumber parsedOriginalNumber, String newNumber) {
+    // Same as Brazil (0/00), carriers: Entel (10), AES (11), Teledata (12), Boliviatel (13)
+    return newNumber;
+  }
+
+  private String reformatForColombia(PhoneNumber parsedOriginalNumber, String newNumber) {
+    // Same as Brazil (0/00), carriers: Orbitel (5), ETB (7), Telecom (9)
+    // Exceptions: 03 (long distance to mobiles), #555 (intl Bellsouth), #999 (intl Comcel)
+    return newNumber;
+  }
+
+  private String reformatForFrance(PhoneNumber parsedOriginalNumber, String newNumber) {
+    // 00 (France Telecom), 40 (Tele 2), 50 (Omnicom), 70 (Le 7 Cegetel), 90 (9 Telecom) - intl only
+    return newNumber;
+  }
+
+  private String reformatForElSalvador(PhoneNumber parsedOriginalNumber, String newNumber) {
+    // 00 (default), 14400 (Telefonica) - intl only
+    return newNumber;
+  }
+
+  private String reformatForGuatemala(PhoneNumber parsedOriginalNumber, String newNumber) {
+    // 00 (default), 130+00 (Telefonica), 147-00 (Telgua) - intl only
+    return newNumber;
+  }
+
+  private String reformatForHongKong(PhoneNumber parsedOriginalNumber, String newNumber) {
+    // 001 (PCCW), 0080 (Hutchinson), 009 (New world) - intl only
+    return newNumber;
+  }
+
+  private String reformatForIsrael(PhoneNumber parsedOriginalNumber, String newNumber) {
+    // 00 (default), 012 (Golden Lines), 013 (Barak LTD), 014 (Bezeq LTD) - intl only
+    return newNumber;
+  }
+
+  private String reformatForJapan(PhoneNumber parsedOriginalNumber, String newNumber) {
+    // 001 (KDD), 010 (MyLine), 0061 (Cable IDC), 0041 (Japan Telecom) - intl only
+    return newNumber;
+  }
+
+  private String reformatForPanama(PhoneNumber parsedOriginalNumber, String newNumber) {
+    // 00 (Cable & Wireless), 088+00 (Telecarrier), 055+00 (Clarocom) - intl only
+    return newNumber;
+  }
+
+  private String reformatForPortugal(PhoneNumber parsedOriginalNumber, String newNumber) {
+    // 00 (default), 882 (Rubicon) - intl only
+    // Madera and Azores - also no code
+    return newNumber;
+  }
+
+  private String reformatForSingapore(PhoneNumber parsedOriginalNumber, String newNumber) {
+    // 001 (Singtel), 002 (MobileOne), 008 (Starhub), 012 (Singtel Faxplus), 013 (Singtel Budgetcall), 018 (Starhub I-Call), 019 (Singtel V019)
+    return newNumber;
+  }
+
+  private String reformatForVenezuela(PhoneNumber parsedOriginalNumber, String newNumber) {
+    // 00 (default), 01-02 (Etelix), 01-07 (multiphone.net.ve), 01-10 (CANTV), 01-11 (Convergence),
+    // 01-14 (Telcel), 01-19 (Totalcom), 01-23 (Orbitel/Entel), 01-50 (LD Telecom), 01-33 (NGTV), 01-99 (Veninfotel)
+    // Intl - same + 0 at end
+    return newNumber;
+  }
 }
