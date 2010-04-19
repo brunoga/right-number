825978a9882e2f99bb95aed53c07415ca2a2ede2
rdamazio <rdamazio@58f3d768-a58c-e14e-36d0-260f8ed6ecdc>
2010-04-19 06:06:31 +0000

    Adding screen in which the user can test number formatting

--- trunk/RightNumber/src/br/com/drzoid/rightnumber/RightNumberActivity.java
+++ trunk/RightNumber/src/br/com/drzoid/rightnumber/RightNumberActivity.java
@@ -1,7 +1,10 @@
 package br.com.drzoid.rightnumber;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.Preference.OnPreferenceClickListener;
 
 /**
  * The main activity, which shows RightNumber preferences.
@@ -12,5 +15,13 @@ public class RightNumberActivity extends PreferenceActivity {
     super.onCreate(savedInstanceState);
 
     addPreferencesFromResource(R.xml.preferences);
+
+    Preference testPreference = findPreference("test_formatting");
+    testPreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+      public boolean onPreferenceClick(Preference preference) {
+        startActivity(new Intent(RightNumberActivity.this, TestNumberActivity.class));
+        return true;
+      }
+    });
   }
 }
---
+++ trunk/RightNumber/src/br/com/drzoid/rightnumber/TestNumberActivity.java
@@ -0,0 +1,129 @@
+package br.com.drzoid.rightnumber;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+/**
+ * Activity which allows the user to test how a number would be formatted.
+ *
+ * @author rdamazio
+ */
+public class TestNumberActivity extends Activity {
+
+  private PhoneNumberFormatter formatter;
+  private Spinner dialingFrom;
+  private Spinner lineFrom;
+  private EditText inputText;
+  private TextView outputText;
+  private String[] countryCodes;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.test_number);
+
+    formatter = new PhoneNumberFormatter(this);
+    countryCodes = getResources().getStringArray(R.array.country_codes);
+
+    // Find relevant widgets
+    dialingFrom = (Spinner) findViewById(R.id.dialing_from);
+    lineFrom = (Spinner) findViewById(R.id.line_from);
+    inputText = (EditText) findViewById(R.id.test_number_input);
+    outputText = (TextView) findViewById(R.id.test_formatted_number);
+
+    // Set default countries to the current ones
+    TelephonyManager telephonyManager =
+        (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+    String originalCountry = telephonyManager.getSimCountryIso().toUpperCase();
+    String currentCountry = telephonyManager.getNetworkCountryIso().toUpperCase();
+    int originalCountryIndex = findCountry(originalCountry, countryCodes);
+    int currentCountryIndex = findCountry(currentCountry, countryCodes);
+    if (originalCountryIndex != -1) {
+      lineFrom.setSelection(originalCountryIndex);
+    }
+    if (currentCountryIndex != -1) {
+      dialingFrom.setSelection(currentCountryIndex);
+    }
+
+    // Attach change listeners to update the formatted number
+    inputText.addTextChangedListener(new TextWatcher() {
+      public void onTextChanged(CharSequence s, int start, int before, int count) {
+        // Do nothing
+      }
+
+      public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        // Do nothing
+      }
+
+      public void afterTextChanged(Editable s) {
+        updateFormattedNumber();
+      }
+    });
+
+    OnItemSelectedListener spinnerListener = new Spinner.OnItemSelectedListener() {
+      public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+        updateFormattedNumber();
+      }
+
+      public void onNothingSelected(AdapterView<?> arg0) {
+        // Do nothing
+      }
+    };
+    dialingFrom.setOnItemSelectedListener(spinnerListener);
+    lineFrom.setOnItemSelectedListener(spinnerListener);
+  }
+
+  /**
+   * Finds the index of a country code in an array of country codes.
+   *
+   * @param country the country code to find
+   * @param countryCodes the complete (unsorted) list of country codes
+   * @return the index where it was found, or -1 if not found
+   */
+  private int findCountry(String country, String[] countryCodes) {
+    for (int i = 0; i < countryCodes.length; i++) {
+      String countryCode = countryCodes[i];
+      if (countryCode.equalsIgnoreCase(country)) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  /**
+   * Updates the formatted number displayed ot the user based on the current
+   * inputs.
+   */
+  private void updateFormattedNumber() {
+    String originalNumber = inputText.getText().toString();
+    if (originalNumber.length() == 0) {
+      outputText.setText(R.string.test_formatted_blank_number);
+      return;
+    }
+
+    int fromCountryIndex = dialingFrom.getSelectedItemPosition();
+    int lineCountryIndex = lineFrom.getSelectedItemPosition();
+    String fromCountryCode = countryCodes[fromCountryIndex];
+    String lineCountryCode = countryCodes[lineCountryIndex];
+
+    Log.d(RightNumberConstants.LOG_TAG,
+        "Number:" + originalNumber + "; from=" + fromCountryCode + "; line=" + lineCountryCode);
+
+    // TODO: Make sure this doesn't show Toasts while the user is typing
+    String newNumber =
+        formatter.formatPhoneNumber(originalNumber, lineCountryCode, fromCountryCode);
+    outputText.setText(newNumber);
+  }
+}
