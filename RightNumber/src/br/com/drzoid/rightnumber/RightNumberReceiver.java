1fbb42bf9396acf6c70b1ef5dc749207419968b5
bruno.albuquerque <bruno.albuquerque@58f3d768-a58c-e14e-36d0-260f8ed6ecdc>
2010-04-19 18:03:45 +0000

    - Dogfood cleanup: Remove debug code that is not needed.
    M    RightNumber/src/br/com/drzoid/rightnumber/RightNumberReceiver.java

--- RightNumber/src/br/com/drzoid/rightnumber/RightNumberReceiver.java
+++ RightNumber/src/br/com/drzoid/rightnumber/RightNumberReceiver.java
@@ -25,22 +25,11 @@ public class RightNumberReceiver extends BroadcastReceiver {
     TelephonyManager telephonyManager =
         (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
 
-    // TODO: Get rid of all the logging
-    Log.d(RightNumberConstants.LOG_TAG, "Line1Number         : " + telephonyManager.getLine1Number());
-    Log.d(RightNumberConstants.LOG_TAG, "NetworkOperator     : " + telephonyManager.getNetworkOperator());
-    Log.d(RightNumberConstants.LOG_TAG, "NetworkOperatorName : " + telephonyManager.getNetworkOperatorName());
-    Log.d(RightNumberConstants.LOG_TAG, "SimOperator         : " + telephonyManager.getSimOperator());
-    Log.d(RightNumberConstants.LOG_TAG, "SimOperatorName     : " + telephonyManager.getSimOperatorName());
-    Log.d(RightNumberConstants.LOG_TAG, "SubscriberId        : " + telephonyManager.getSubscriberId());
-
     String currentCountry = telephonyManager.getNetworkCountryIso().toUpperCase();
     Log.d(RightNumberConstants.LOG_TAG, "Current country  : " + currentCountry);
 
     String originalCountry = telephonyManager.getSimCountryIso().toUpperCase();
     Log.d(RightNumberConstants.LOG_TAG, "Original country : " + originalCountry);
-
-    boolean isRoaming = telephonyManager.isNetworkRoaming();
-    Log.d(RightNumberConstants.LOG_TAG, "Roaming          : " + isRoaming);
 
     String originalNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
     Log.d(RightNumberConstants.LOG_TAG, "Original number : " + originalNumber);
@@ -57,5 +46,4 @@ public class RightNumberReceiver extends BroadcastReceiver {
 
     setResultData(newNumber);
   }
-
 }
