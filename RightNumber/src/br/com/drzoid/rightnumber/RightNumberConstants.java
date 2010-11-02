29bbb88ce3ae31e9b0ce7022e78d4c6fbfb3633d
bruno.albuquerque@gmail.com <bruno.albuquerque@gmail.com@58f3d768-a58c-e14e-36d0-260f8ed6ecdc>
2010-11-02 12:38:26 +0000

    - Added "International mode". A.k.a. Google-Voice friendly mode. In this mode,   all numbers will be formatted with the international format instead of the   local calling format. - Some small cleanups here and there.

--- RightNumber/src/br/com/drzoid/rightnumber/RightNumberConstants.java
+++ RightNumber/src/br/com/drzoid/rightnumber/RightNumberConstants.java
@@ -7,6 +7,10 @@ public class RightNumberConstants {
 
   // Preference keys
   public static final String ENABLE_FORMATTING = "enable_formatting";
+  public static final String ENABLE_INTERNATIONAL_MODE = "enable_international_mode";
+  public static final String CARRIERS = "carriers";  
+  public static final String TEST_FORMATTING = "test_formatting";
+  
   public static final String ENABLE_CARRIER_BASE_KEY = "carrier_enable_";
 
   private RightNumberConstants() { }
