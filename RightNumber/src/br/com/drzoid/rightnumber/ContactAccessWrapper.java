cf01c55512986273cecf5e9a67fbe6a816f44b33
rdamazio <rdamazio@58f3d768-a58c-e14e-36d0-260f8ed6ecdc>
2010-12-31 19:49:21 +0000

    Fixing batch application in pre-eclair devices

--- RightNumber/src/br/com/drzoid/rightnumber/ContactAccessWrapper.java
+++ RightNumber/src/br/com/drzoid/rightnumber/ContactAccessWrapper.java
@@ -15,9 +15,18 @@
  */
 package br.com.drzoid.rightnumber;
 
+import java.util.ArrayList;
+
+import android.content.ContentProviderOperation;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.OperationApplicationException;
 import android.net.Uri;
+import android.os.RemoteException;
 import android.provider.Contacts;
 import android.provider.ContactsContract;
+import android.util.Log;
 
 /**
  * A thin wrapper around contact access classes, which changed in API level 5.
@@ -26,24 +35,38 @@ import android.provider.ContactsContract;
  */
 @SuppressWarnings("deprecation")
 public abstract class ContactAccessWrapper {
+  /**
+   * Interface for building operation batches and applying them, in an API-level-safe way.
+   */
+  public interface OperationBatchBuilder {
+    /** Adds an update to a single string column. */
+    boolean addUpdate(Uri updateUri, String column, String value);
 
-  public abstract String getAuthority();
+    /** Applies all added updates. */
+    boolean apply();
+
+    /** Returns the current number of accumulated changes. */
+    int getNumPendingChanges();
+  }
+
   public abstract Uri getPhoneUri();
   public abstract String getPhoneIdColumn();
   public abstract String getPhoneNumberColumn();
+  public abstract OperationBatchBuilder newBatchBuilder(int expectedSize);
 
-  public static ContactAccessWrapper create() {
+  public static ContactAccessWrapper create(Context context) {
     if (RightNumberConstants.ANDROID_API_LEVEL >= 5) {
-      return new EclairAccessWrapper();
+      return new EclairAccessWrapper(context);
     } else {
-      return new CupcakeAccessWrapper();
+      return new CupcakeAccessWrapper(context);
     }
   }
 
   private static class EclairAccessWrapper extends ContactAccessWrapper {
-    @Override
-    public String getAuthority() {
-      return ContactsContract.AUTHORITY;
+    private final Context context;
+
+    public EclairAccessWrapper(Context context) {
+      this.context = context;
     }
 
     @Override
@@ -60,12 +83,65 @@ public abstract class ContactAccessWrapper {
     public String getPhoneNumberColumn() {
       return ContactsContract.CommonDataKinds.Phone.NUMBER;
     }
+
+    /**
+     * Eclair+ version of the batch builder.
+     * This relies on {@link ContentResolver#applyBatch}.
+     */
+    private static class EclairOperationBatchBuilder implements OperationBatchBuilder {
+      private final ArrayList<ContentProviderOperation> operations;
+      private final ContentResolver resolver;
+
+      public EclairOperationBatchBuilder(ContentResolver resolver, int expectedSize) {
+        this.resolver = resolver;
+        this.operations = new ArrayList<ContentProviderOperation>(expectedSize);
+      }
+
+      @Override
+      public boolean addUpdate(Uri updateUri, String column, String value) {
+        operations.add(ContentProviderOperation.newUpdate(updateUri)
+            .withValue(column, value)
+            .build());
+        return true;
+      }
+
+      @Override
+      public boolean apply() {
+        if (operations.isEmpty()) return true;
+
+        try {
+          Log.d(RightNumberConstants.LOG_TAG, "Applying " + operations.size() + " changes");
+          resolver.applyBatch(ContactsContract.AUTHORITY, operations);
+          Log.d(RightNumberConstants.LOG_TAG, "Changes applied");
+          return true;
+        } catch (RemoteException e) {
+          Log.e(RightNumberConstants.LOG_TAG, "Failed to apply changes", e);
+          return false;
+        } catch (OperationApplicationException e) {
+          Log.e(RightNumberConstants.LOG_TAG, "Failed to apply changes", e);
+          return false;
+        } finally {
+          operations.clear();
+        }
+      }
+
+      @Override
+      public int getNumPendingChanges() {
+        return operations.size();
+      }
+    }
+
+    @Override
+    public OperationBatchBuilder newBatchBuilder(int expectedSize) {
+      return new EclairOperationBatchBuilder(context.getContentResolver(), expectedSize);
+    }
   }
 
   private static class CupcakeAccessWrapper extends ContactAccessWrapper {
-    @Override
-    public String getAuthority() {
-      return Contacts.AUTHORITY;
+    private final Context context;
+
+    public CupcakeAccessWrapper(Context context) {
+      this.context = context;
     }
 
     @Override
@@ -81,6 +157,50 @@ public abstract class ContactAccessWrapper {
     @Override
     public String getPhoneNumberColumn() {
       return Contacts.Phones.NUMBER;
+    }
+
+    /**
+     * Cupcake version of the batch builder.
+     * Since API level < 5 doesn't support batch updates, we apply each operation
+     * immediately.
+     */
+    private class CupcakeOperationBatchBuilder implements OperationBatchBuilder {
+      private final ContentResolver resolver;
+
+      public CupcakeOperationBatchBuilder(ContentResolver resolver) {
+        this.resolver = resolver;
+      }
+
+      @Override
+      public boolean addUpdate(Uri updateUri, String column, String value) {
+        // Apply immediately
+        ContentValues values = new ContentValues(1);
+        values.put(column, value);
+        int numUpdates = resolver.update(updateUri, values, null, null);
+
+        if (numUpdates < 1) {
+          Log.e(RightNumberConstants.LOG_TAG, "Change not applied to URI " + updateUri);
+          return false;
+        }
+        return true;
+      }
+
+      @Override
+      public boolean apply() {
+        // Do nothing
+        return true;
+      }
+
+      @Override
+      public int getNumPendingChanges() {
+        // Nothing is ever accumulated here.
+        return 0;
+      }
+    }
+
+    @Override
+    public OperationBatchBuilder newBatchBuilder(int expectedSize) {
+      return new CupcakeOperationBatchBuilder(context.getContentResolver());
     }
   }
 }
